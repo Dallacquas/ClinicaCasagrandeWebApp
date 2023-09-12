c07b02f9d56e963d5db64499446b2511504031d4
Dallacquas <dallacqua.familia@gmail.com>
2023-09-11 21:16:12 -0300

    first commit

---
+++ src/app/pages/definitions/definitions.component.ts
@@ -0,0 +1,13 @@
+import { PerfilService } from 'src/app/services/perfil/perfil.service';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Perfil } from 'src/app/models/Perfils';
+
+@Component({
+  selector: 'app-definitions',
+  templateUrl: './definitions.component.html',
+  styleUrls: ['./definitions.component.css']
+})
+export class DefinitionsComponent {
+
+}
