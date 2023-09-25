5f4ea827c335a8d053a23c4080cc39ad2afec0a3
Dallacquas <dallacqua.familia@gmail.com>
2023-09-25 12:49:14 -0300

    Atualizado 25-09

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,11 +1,12 @@
 
+export const environment = {
+  production: false,
+  ApiUrl: 'http://clinica-api/api'
+};
+
+
 // export const environment = {
-//     production: false,
-//     ApiUrl: 'http://clinica-api/api'
+
+//   ApiUrl: "https://localhost:7298/api"
 // };
 
-
-export const environment = {
-
-  ApiUrl: "https://localhost:7298/api"
-};
