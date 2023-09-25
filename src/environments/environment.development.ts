2e2cce208a90dfd67ec5d51502678f6783f805a2
Dallacquas <dallacqua.familia@gmail.com>
2023-09-24 22:42:04 -0300

    Alterado o Environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 
-// export const environment = {
-//     production: false,
-//     ApiUrl: 'http://clinica-api/api'
-// };
-
-
 export const environment = {
-
-  ApiUrl: "https://localhost:7298/api"
+  production: false,
+  ApiUrl: 'http://clinica-api/api'
 };
+
+
+// export const environment = {
+
+//   ApiUrl: "https://localhost:7298/api"
+// };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 
 
-// export const environment = {
-//     production: false,
-//     ApiUrl: 'http://clinica-api/api'
-// };
-
-
 export const environment = {
-
-  ApiUrl: "https://localhost:7298/api"
+    production: false,
+    ApiUrl: 'http://clinica-api/api'
 };
+
+
+// export const environment = {
+
+//   ApiUrl: "https://localhost:7298/api"
+// };
 
 
 /*
